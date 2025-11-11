896704e732c24e9af078c0cfde4b16936a9eaeb1
Zeyum3 <loicminnoye@gmail.com>
2025-11-11 13:57:02 +0100

    MULTIPLAYER ADDED

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
-
-public class PlayerCtrl : MonoBehaviour
+using Unity.Netcode;
+public class PlayerCtrl : NetworkBehaviour
 {
     public float movSpeed;
     float speedX, speedY;
     Rigidbody2D rb;
        
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the NetworkBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,12 +15,16 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
-        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
-        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
-        rb.linearVelocity = new Vector2 (speedX, speedY);
+        if (!IsOwner) return;
+        Move();
         
 
 
+    }
+    private void Move()
+        {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.linearVelocity = new Vector2(speedX, speedY);
     }
 }
